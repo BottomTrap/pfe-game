6f22533b55e1055e2f78ffd75f473e2899da4ac7
BottomTrap <riadhgharbi96@gmail.com>
2019-03-19 15:54:50 +0100

    Reverting back to old stuff

--- Test/Assets/Scripts/PlayerMovement.cs
+++ Test/Assets/Scripts/PlayerMovement.cs
@@ -2,38 +2,78 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(PlayerStats))]
+[RequireComponent(typeof(CharacterController))]
+//[RequireComponent(typeof(Animator))]
 public class PlayerMovement : MonoBehaviour
 {
     private CharacterController characterController;
-    
+    private Animator animator;
 
     [SerializeField]
     private float moveSpeed = 30;
     [SerializeField]
-    private float turnSpeed = 5f;
+    private float turnSpeed = 3.5f;
+
 
     private Vector3 lastPosition;
     private float distanceTraveled;
+    public bool didHit = true;
+    private bool aiming = false;
 
     private PlayerStats playerstats;
+
+
+    public delegate void ViewChangeDelegate();
+    public static event ViewChangeDelegate viewChangeDelegate;
     private void Awake()
     {
         playerstats = (PlayerStats)GetComponent(typeof(PlayerStats));
         characterController = GetComponent<CharacterController>();
+        animator = GetComponentInChildren<Animator>();
         lastPosition = transform.position;
     }
 
-    private void Update()
+    private void FixedUpdate()
     {
         if (ObjectClick.objectClicked)
         {
+            //didHit = false;   // IT GETS CALLED EVERYTIME, WONT WORK
             distanceTraveled += Vector3.Distance(transform.position, lastPosition);
             lastPosition = transform.position;
-            if (distanceTraveled < playerstats.AP.BaseValue)
+            Rotate();
+            if (distanceTraveled < playerstats.AP.BaseValue * 10f)
             {
                 Movement();
             }
+            if (!didHit)
+            {
+                if (Input.GetKeyDown(KeyCode.R) && !aiming)
+                {
+                    Attack();
+                }
+                if (Input.GetKeyDown(KeyCode.Tab))
+                {
+                    aiming = !aiming;
+                    
+                    RangedAttack();
+                }
+
+            }
         }
+    }
+    void Attack()
+    {
+        animator.SetTrigger("Attack");
+        didHit = true;
+    }
+    void RangedAttack()
+    {
+        //Get to the aim view
+        //Get to the aim control
+        //then push R button or Attack button elli houa
+        viewChangeDelegate();
+        aiming = !aiming;
     }
     void Movement()
     {
@@ -47,6 +87,15 @@ public class PlayerMovement : MonoBehaviour
 
 
 
+        
+    }
+    void Rotate()
+    {
+        var horizontal = Input.GetAxis("Horizontal");
+        var vertical = Input.GetAxis("Vertical");
+
+        var movement = new Vector3(horizontal, 0, vertical);
+        movement = transform.TransformDirection(movement);
         if (movement.magnitude > 0)
         {
             Quaternion newDirection = Quaternion.LookRotation(movement);
