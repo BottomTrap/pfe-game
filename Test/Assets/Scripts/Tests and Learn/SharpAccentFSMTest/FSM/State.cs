007d67259be464df9b1eee9a02e0317a58a92c6b
BottomTrap <riadhgharbi96@gmail.com>
2019-03-23 12:18:48 +0100

    More implementation
    Check previous history comment.

--- Test/Assets/Scripts/Tests and Learn/SharpAccentFSMTest/FSM/State.cs
+++ Test/Assets/Scripts/Tests and Learn/SharpAccentFSMTest/FSM/State.cs
@@ -17,6 +17,31 @@ namespace RG
                 this.updateActions = updateActions;
                 this.fixedActions = fixedUpdateActions;
             }
+
+            public void FixedTick()
+            {
+                ExecuteActionsArray(fixedActions);
+            }
+
+            public void Tick()
+            {
+                ExecuteActionsArray(updateActions);
+                forceSkip = false;
+            }
+
+            void ExecuteActionsArray(StateAction[] list)
+            {
+                if (list == null)
+                    return;
+
+                for (int i = 0; i < list.Length; i++)
+                {
+                    if (forceSkip)
+                        break;
+
+                    forceSkip = list[i].Execute();
+                }
+            }
         }
 
 }
