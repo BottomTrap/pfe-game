a3cda6d0c96ce43181daa58d0bab96cbce1df748
BottomTrap <riadhgharbi96@gmail.com>
2019-03-23 12:03:18 +0100

    Continuation of FSM Implementation
    Continuting from the SharpAccent Tutorial
    Seems well made and highly reusable. I need to analyze and understand the code more but it's simple and lightweight.
    It will take me a few days to implement my current code using that.
    Where I stopped:
    https://github.com/sharpaccent/Lightweight-State-Machine/blob/master/Scripts/FSM/State.cs

---
+++ Test/Assets/Scripts/Tests and Learn/SharpAccentFSMTest/FSM/State.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RG
+{
+        public class State
+        {
+            private StateAction[] updateActions;
+            private StateAction[] fixedActions;
+
+            private bool forceSkip;
+
+
+            public State(StateAction[] updateActions, StateAction[] fixedUpdateActions)
+            {
+                this.updateActions = updateActions;
+                this.fixedActions = fixedUpdateActions;
+            }
+        }
+
+}
